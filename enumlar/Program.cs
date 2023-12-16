8f562aba85d787a4275a93e9c381272761ac3813
Feyzullah Durası <durasifeyzullah672@gmail.com>
2023-12-16 15:15:18 +0300

    enum week example

---
+++ enumlar/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    enum Gunler
+    {
+        Pazartesi = 1,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Lütfen bir gün seçiniz:");
+
+        foreach (var gun in Enum.GetValues(typeof(Gunler)))
+        {
+            Console.WriteLine($"{(int)gun}. {gun}");
+        }
+
+        int secilenGunNumarasi;
+        bool gecerliGiris = false;
+
+        do
+        {
+            Console.Write("Gün numarasını giriniz: ");
+
+            if (int.TryParse(Console.ReadLine(), out secilenGunNumarasi))
+            {
+                if (Enum.IsDefined(typeof(Gunler), secilenGunNumarasi))
+                {
+                    gecerliGiris = true;
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz gün numarası! Lütfen tekrar deneyin.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+            }
+        } while (!gecerliGiris);
+
+        Gunler secilenGun = (Gunler)secilenGunNumarasi;
+
+        Console.WriteLine($"Seçilen gün: {secilenGun}");
+
+        if (secilenGun >= Gunler.Pazartesi && secilenGun <= Gunler.Cuma)
+        {
+            Console.WriteLine("Bu gün bir hafta içi günüdür.");
+        }
+        else
+        {
+            Console.WriteLine("Bu gün bir hafta sonu günüdür.");
+        }
+    }
+}
